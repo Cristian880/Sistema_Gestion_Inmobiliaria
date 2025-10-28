ceba17d4edb5d79c9ecf29af5116d39dd21f00a6
Cristian Luna <cristianlunarosario08@gmail.com>
2025-10-28 00:25:11 -0400

    camnios en la capa de persistencia, seguir despues

---
+++ ClassLibrary4/Contexts/Sis_InmobiliariaAppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sis_Inmobiliaria.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sis_Inmobiliaria.Infrastructure.Persistence.Contexts
+{
+    public class InvestmentAppContext : DbContext
+    {
+        public InvestmentAppContext(DbContextOptions<InvestmentAppContext> options) : base(options) { }
+        public DbSet<PropertyStatus> PropertyStatus { get; set; }
+        public DbSet<PropertyType> PropertyType { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); //Liskov-substitution
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ ClassLibrary4/EntityConfigurations/PropertyTypeEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sis_Inmobiliaria.Core.Domain.Entities;
+
+namespace Sis_Inmobiliaria.Infrastructure.Persistence.EntityConfigurations
+{
+    internal class PropertyTypeEntityConfiguration
+    {
+        public void Configure(EntityTypeBuilder<PropertyType> builder)
+        {
+            #region Basic configuration
+            builder.HasKey(x => x.Id);
+            builder.ToTable("PropertyType");
+            #endregion
+
+            #region Property configurations
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(255);
+            #endregion
+
+            #region relationships
+
+            builder.WithOne(a => a.PropertyStatus)
+                .HasForeignKey(a => a.PropertyStatusId)
+                .OnDelete(DeleteBehavior.Cascade);//lambda
+            #endregion
+        }
+    }
+}
